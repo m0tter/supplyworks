bf074794d42337c38be7768624d3d2d3535b0e79
m0tter <qldscottie@gmail.com>
2017-04-20 06:34:29 +1000

    register update

--- client/src/app/employer/register/register.component.ts
+++ client/src/app/employer/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Employer, User, Address } from 'supplyworks';
 
+import { RegisterService } from '../services'
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -13,8 +15,9 @@ export class RegisterComponent implements OnInit {
   public adminUser: User;
   public address: Address;
   public registerForm: FormGroup;
+  public registering = false;
 
-  constructor( private fb: FormBuilder ) {
+  constructor( private fb: FormBuilder, private registerService: RegisterService ) {
     this.employer = {name: '', address: [], contactId: '', employeeId: [], casualId: []};
     this.adminUser = {email: '', firstName: '', employerId: '', isAdmin: true, lastName: '', mobilePhone: '', password: ''};
     this.address = {country: '', line1: '', line2: '', state: '', suburb: ''};
@@ -23,21 +26,23 @@ export class RegisterComponent implements OnInit {
 
   buildForm(): void {
     this.registerForm = this.fb.group({
-    schoolName: [this.employer.name, Validators.required],
-    firstName: [this.adminUser.firstName, Validators.required],
-    lastName: [this.adminUser.lastName, Validators.required],
-    email: [this.adminUser.email, Validators.required],
-    password: [this.adminUser.password, Validators.required],
-    phone: [this.adminUser.mobilePhone, Validators.required],
-    addrLine1: [this.address.line1],
-    addrLine2: [this.address.line2],
-    suburb: [this.address.suburb]
+    schoolName:   [this.employer.name, Validators.required],
+    firstName:    [this.adminUser.firstName, Validators.required],
+    lastName:     [this.adminUser.lastName, Validators.required],
+    email:        [this.adminUser.email, Validators.required],
+    password:     [this.adminUser.password, Validators.required],
+    phone:        [this.adminUser.mobilePhone, Validators.required],
+    addrLine1:    [this.address.line1],
+    addrLine2:    [this.address.line2],
+    suburb:       [this.address.suburb]
     });
   }
 
   doRegister(): void {
+    this.registering = true;
     if(this.registerForm.dirty && this.registerForm.valid) {
-      console.log("do register");
+      this.registerService.Register(this.employer, this.adminUser)
+        .then(res => { })
     }
   }
 
